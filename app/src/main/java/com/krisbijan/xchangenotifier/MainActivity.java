6c80875c5772afc91116767b862de08a4c368e3f
ekribil <kristijan.bilic@ericsson.com>
2017-12-13 09:16:13 +0100

    first commit

---
+++ app/src/main/java/com/krisbijan/xchangenotifier/MainActivity.java
@@ -0,0 +1,13 @@
+package com.krisbijan.xchangenotifier;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
